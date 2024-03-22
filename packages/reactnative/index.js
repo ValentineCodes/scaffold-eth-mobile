b18f0ee3d681ccb389d6e12f1d19cc84fbd1ee4b
Valentine Orga <valentineorga@gmail.com>
2024-03-22 22:13:05 +0100

    redux store setup

--- packages/reactnative/index.js
+++ packages/reactnative/index.js
@@ -4,8 +4,27 @@
 
 import 'react-native-url-polyfill/auto';
 import "fast-text-encoding"
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './src/screens/App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { NativeBaseProvider } from 'native-base';
+import { Provider } from 'react-redux';
+import { ToastProvider } from 'react-native-toast-notifications'
+import { persistor, store } from './src/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
-AppRegistry.registerComponent(appName, () => App);
+function Application() {
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <ToastProvider>
+                    <NativeBaseProvider>
+                        <App />
+                    </NativeBaseProvider>
+                </ToastProvider>
+            </PersistGate>
+        </Provider>
+    );
+}
+
+AppRegistry.registerComponent(appName, () => Application);
