851c6bd14ff4ee76c1d904f326ebd888bf8cba27
Valentine Orga <valentineorga@gmail.com>
2024-04-02 09:21:15 +0100

    created hook to use connected network info

---
+++ packages/reactnative/src/hooks/scaffold-eth/useNetwork.ts
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Network } from '../../store/reducers/Networks'
+
+export default function useNetwork() {
+    const connectedNetwork: Network = useSelector((state: any) => state.networks.find((network: Network) => network.isConnected))
+
+    return connectedNetwork
+}
--- packages/reactnative/src/screens/Main/Tab/Home.tsx
+++ packages/reactnative/src/screens/Main/Tab/Home.tsx
@@ -1,13 +1,15 @@
 import { View } from 'native-base'
 import React from 'react'
 import { useDeployedContractInfo } from '../../../hooks/scaffold-eth/useDeployedContractInfo'
+import useNetwork from '../../../hooks/scaffold-eth/useNetwork'
 
 type Props = {}
 
 export default function Home({ }: Props) {
     const { data, isLoading } = useDeployedContractInfo("YourContract")
+    const network = useNetwork()
 
     return (
-        <View></View>
+        <View flex={"1"} bgColor={"white"}></View>
     )
 }
