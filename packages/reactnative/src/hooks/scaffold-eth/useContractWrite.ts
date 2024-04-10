d339af9616ae812465490000685806b1ecc13e25
Valentine Orga <valentineorga@gmail.com>
2024-04-10 17:14:04 +0100

    added wallet and gasLimit to write transaction

--- packages/reactnative/src/hooks/scaffold-eth/useContractWrite.ts
+++ packages/reactnative/src/hooks/scaffold-eth/useContractWrite.ts
@@ -20,6 +20,7 @@ interface UseWriteConfig {
     args?: any[]
     value?: BigNumber | undefined
     blockConfirmations?: number;
+    gasLimit?: BigNumber | undefined
 }
 
 interface SendTxConfig {
@@ -36,6 +37,7 @@ interface SendTxConfig {
  * @param config.args - arguments for the function
  * @param config.value - value in ETH that will be sent with transaction
  * @param config.blockConfirmations - number of block confirmations to wait for (default: 1)
+ * @param config.gasLimit - transaction gas limit
  */
 export default function useContractWrite({
     abi,
@@ -43,10 +45,12 @@ export default function useContractWrite({
     functionName,
     args,
     value,
-    blockConfirmations
+    blockConfirmations,
+    gasLimit
 }: UseWriteConfig) {
     const writeArgs = args
     const writeValue = value
+    const _gasLimit = gasLimit || 21000
 
     const {openModal} = useModal()
     const network = useNetwork()
@@ -75,13 +79,22 @@ export default function useContractWrite({
             throw new Error("You are on the wrong network")
         }
 
-        return new Promise((resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
             try {
                 const provider = new ethers.providers.JsonRpcProvider(network.provider)
     
-                const contract = new ethers.Contract(address, abi, provider)
+                const accounts = await SInfo.getItem("accounts", {
+                    sharedPreferencesName: "sern.android.storage",
+                    keychainService: "sern.ios.storage",
+                })
+        
+                const activeAccount: Wallet = Array.from(JSON.parse(accounts)).find(account => account.address.toLowerCase() == connectedAccount.address.toLowerCase())
+        
+                const wallet = new ethers.Wallet(activeAccount.privateKey).connect(provider)
+    
+                const contract = new ethers.Contract(address, abi, wallet)
 
-                openModal("SignTransactionModal", {contract, contractAddress: address, functionName, args: _args, value: _value, onConfirm})
+                openModal("SignTransactionModal", {contract, contractAddress: address, functionName, args: _args, value: _value, gasLimit: _gasLimit, onConfirm})
                 
             } catch(error) {
                 reject(error)
@@ -104,7 +117,8 @@ export default function useContractWrite({
                     const contract = new ethers.Contract(address, abi, wallet)
         
                     const tx = await contract.functions[functionName](..._args, {
-                        value: _value
+                        value: _value,
+                        gasLimit: _gasLimit
                     })
                     const receipt = await tx.wait(blockConfirmations || 1)
                     toast.show("Transaction Successful!", {
