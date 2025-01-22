4059ec6a1ea0e4d69d3b988f5a2724d6be320cc9
Valentine Orga <valentineorga@gmail.com>
2025-01-22 08:47:32 +0100

    chore: setup react-native-paper

--- packages/reactnative/index.js
+++ packages/reactnative/index.js
@@ -4,10 +4,10 @@
 
 import 'react-native-url-polyfill/auto';
 import 'fast-text-encoding';
-import { NativeBaseProvider } from 'native-base';
 import { AppRegistry } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 import { ToastProvider } from 'react-native-toast-notifications';
-import { Provider } from 'react-redux';
+import { Provider as StoreProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { name as appName } from './app.json';
 import App from './src/screens/App';
@@ -15,15 +15,13 @@ import { persistor, store } from './src/store';
 
 function Application() {
   return (
-    <Provider store={store}>
+    <StoreProvider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <ToastProvider>
-          <NativeBaseProvider>
-            <App />
-          </NativeBaseProvider>
+          <PaperProvider></PaperProvider>
         </ToastProvider>
       </PersistGate>
-    </Provider>
+    </StoreProvider>
   );
 }
 
