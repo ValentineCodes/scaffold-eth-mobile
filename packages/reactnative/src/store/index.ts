de72dbcc62fe4b4409911e0a7a77a42b1eb1ce77
Valentine Orga <valentineorga@gmail.com>
2025-02-19 15:50:40 +0100

    chore: persist storage

--- packages/reactnative/src/store/index.ts
+++ packages/reactnative/src/store/index.ts
@@ -23,7 +23,7 @@ const persistConfig = {
   key: 'root',
   version: 1,
   storage: AsyncStorage,
-  blacklist: ['balance', 'networks', 'transactions']
+  blacklist: ['balance', 'networks']
 };
 
 const reducers = combineReducers({
